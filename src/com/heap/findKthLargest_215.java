63978ff83cf1040c7c7e356cd423ddf8227f99ae
HZL <han.zhulin@outlook.com>
2021-08-22 14:41:18 +0800

    topK

--- src/com/heap/findKthLargest_215.java
+++ src/com/heap/findKthLargest_215.java
@@ -1,6 +1,9 @@
 package com.heap;
 
+import java.util.Arrays;
+
 public class findKthLargest_215 {
+    /*
     public static class MaxHeap {
         int size = 0;
         int maxSize;
@@ -16,33 +19,76 @@ public class findKthLargest_215 {
         }
     }
 
-    public int findKthLargest(int[] nums, int k) {
-        buildMaxHeap(nums, nums.length);
 
-        MaxHeap maxHeap = new MaxHeap(10 ^ 4);
-//        if(maxHeap.isFull(maxHeap)) return;
-        int index = ++maxHeap.size;
 
-        for (int i : nums) {
-            maxHeap.elements[maxHeap.size++] = i;
-
+    //
+    public static void percDown(int[] nums, int p) {
+        int parent, child;
+        int tmp = nums[p];
+        for (parent = p; parent * 2 <= nums.length; parent = child) {
+            child = parent * 2;
+            if (child != nums.length && nums[child] < nums[child + 1]) {
+                child++;
+            }
+            if (tmp >= nums[child]) break;
+            else nums[parent] = nums[child];
         }
-
-
+        nums[parent] = tmp;
     }
 
-    public void buildMaxHeap(int[] nums, int size) {
-        int[] heap = new int[size];
-        int heapsize = 0;
-        heap[0] = 10 ^ 4;
-        for (int i : nums) {
-            heap[++heapsize] = i;
-            int index = heapsize;
-            if (heap[index] > heap[index / 2]) {
-                int tmp = heap[index / 2];
-                heap[index / 2] = heap[index];
-                heap[index] = tmp;
-            }
+    public static void buildHeapPerc(int[] nums) {
+        int i;
+        for (i = nums.length / 2; i > 0; i--)
+            percDown(nums, i);
+        System.out.println(Arrays.toString(nums));
+    }
+    */
+
+    public static int findKthLargest(int[] nums, int k) {
+        int size=nums.length;
+        int[] heap = buildMaxHeap(nums, size);
+        System.out.println(Arrays.toString(heap));
+        int max = heap[1];
+        for (int i = 1; i < k; i++) {
+            deleteMax(heap, size);
+            size--;
         }
+        System.out.println(Arrays.toString(heap));
+        return heap[1];
+    }
+    //
+    public static int[] buildMaxHeap(int[] nums, int size) {
+        int[] heap = new int[size + 1];
+        int heapSize = 0;
+        heap[heapSize] = Integer.MAX_VALUE;
+        for (int i : nums) {
+//            heap[++heapSize] = i;
+            int index = ++heapSize;
+            for (; i > heap[index / 2]; index = index / 2) {
+                heap[index] = heap[index / 2];
+            }
+            heap[index] = i;
+        }
+        return heap;
+    }
+
+    public static void deleteMax(int[] nums, int size) {
+        int parent, child;
+        int tmp = nums[size];
+        for (parent = 1; parent * 2 <= size; parent = child) {
+            child = parent * 2;
+            if ((child != size) && nums[child] < nums[child + 1]) {
+                child++;
+            }
+            if (tmp >= nums[child]) break;
+            else nums[parent] = nums[child];
+        }
+        nums[parent] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,1};
+        int kthLargest = findKthLargest(nums, 2);
+        System.out.println(kthLargest);
     }
 }
